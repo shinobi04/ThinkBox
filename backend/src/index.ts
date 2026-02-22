7c6eb7cb2f512c0109e5a215e9fbe097c75e8557
Anurag Kumar Singh <anuragkrsingh3456@gmail.com>
2026-02-22 23:41:26 +0530

    feat: Set up Prisma ORM with database schema for users, notes, and authentication, and integrate better-auth.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,12 @@ import express, { type Request, type Response } from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
+import { auth } from './lib/auth';
+import { toNodeHandler } from 'better-auth/node';
+
 app.use(express.json());
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello from Bun + Express + TS!');
